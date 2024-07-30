ae59cd43388a3c6cbb6e5785869e817e1939de55
a2davl1 <avielarenas1@gmail.com>
2024-07-30 11:21:32 -0400

    first program 2

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -14,7 +14,7 @@ public class Exercise_03 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
         System.out.println("Check out my first program!");
         // write code to print out "Check out my first Java program!" below
 
