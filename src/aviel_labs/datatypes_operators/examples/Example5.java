f988663fc9cb160b86626b22e375d9fc916c72a6
a2davl1 <avielarenas1@gmail.com>
2024-10-08 12:14:18 -0400

    commiting notes taken from example 5.java

--- src/aviel_labs/datatypes_operators/examples/Example5.java
+++ src/aviel_labs/datatypes_operators/examples/Example5.java
@@ -15,3 +15,17 @@ class Sound {
                 " feet away.");
     }
 }
+
+/*
+    Class Declaration:
+        the sound class contains the main method
+    main method
+        variable declaration
+            double dist; declares a variable dist of type double to store the distance
+        distance calculation
+            dist = 7.2 * 100;
+            calculates the distance based on the time it takes for the sound of lightning to travel.
+            in this case, 7.2 seconds is multiplied by 1100, the speed of sound in feet per second
+        output
+            prints the calculated distance, showing how far away the lightning is based on the time it took for the sound to reach you
+ */
