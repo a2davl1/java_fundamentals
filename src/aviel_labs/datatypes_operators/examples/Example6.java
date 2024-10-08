e1f339abc6c89ba9a4348721c989cc6082b2ae32
a2davl1 <avielarenas1@gmail.com>
2024-10-08 12:29:54 -0400

    Commiting notes taken on Example6.java

--- src/aviel_labs/datatypes_operators/examples/Example6.java
+++ src/aviel_labs/datatypes_operators/examples/Example6.java
@@ -8,3 +8,35 @@ class StrDemo {
         System.out.println("D\tE\tF");
     }
 }
+
+/*
+
+Class declaration
+    StrDemo class contains the main method
+    Main Method
+        First Print Statement
+        System.out.println("First line\nSecond line");
+            Result:
+                First line
+                Second line
+        second print statement
+        System.out.println("A\tB\tC");
+            Output: "A", "B", and "C" are separated by tabs because of the \t (tab) character.
+            Result:
+                A    B    C
+        Third Print Statement
+        System.out.println("D\tE\tF");
+           Output: "D", "E", and "F" are separated by tabs.
+           Results:
+                D    E    F
+
+Key Points:
+Escape Sequences: The \n and \t are escape sequences in Java.
+\n inserts a newline.
+\t inserts a tab space.
+System.out.println(): This method is used to print text to the console.
+
+
+
+
+ */
