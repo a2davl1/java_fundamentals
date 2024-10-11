fdd4dacd499eef8c1b6171640c3e69a89284e71f
a2davl1 <avielarenas1@gmail.com>
2024-10-11 09:56:22 -0400

    Commiting notes, including outputs on Example4.java

--- src/aviel_labs/datatypes_operators/examples/Example4.java
+++ src/aviel_labs/datatypes_operators/examples/Example4.java
@@ -20,3 +20,42 @@ class BoolDemo {
         System.out.println("10 > 9 is " + (10 > 9));
     }
 }
+
+/*
+
+Output:
+b is false
+b is true
+This is executed.
+10 > 9 is true
+
+ */
+
+/*
+    Class Declaration:
+        The BoolDemo class contains the main method.
+    Main Method: This is where the program execution begins.
+        Variable Declaration:
+            boolean b; declares a boolean variable b.
+        Variable Initialization:
+            b = false; assigns the value false to b.
+            System.out.println("b is " + b); prints "b is false".
+            b = true; assigns the value true to b.
+            System.out.println("b is " + b); prints "b is true".
+        Boolean Control in if Statement:
+            if(b) System.out.println("This is executed.");
+                Since b is true, this statement prints "This is executed."
+            b = false; assigns the value false to b.
+            if(b) System.out.println("This is not executed.");
+                Since b is now false, this statement is not executed.
+        Relational Operator:
+            System.out.println("10 > 9 is " + (10 > 9));
+                The expression (10 > 9) evaluates to true, so this statement prints "10 > 9 is true".
+Key Points:
+Boolean Variables: The boolean data type can hold one of two values: true or false.
+
+Control Statements: Boolean values can control the flow of if statements.
+
+Relational Operators: The result of a relational operation (e.g., 10 > 9) is a boolean value.
+
+ */
