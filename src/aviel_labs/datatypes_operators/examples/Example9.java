1096ba46ac370646ed7e03b30245633004947199
a2davl1 <avielarenas1@gmail.com>
2024-10-11 09:40:21 -0400

    Commiting notes taken on Example9.java

--- src/aviel_labs/datatypes_operators/examples/Example9.java
+++ src/aviel_labs/datatypes_operators/examples/Example9.java
@@ -13,3 +13,26 @@ class VarInitDemo {
         }
     }
 }
+
+/*
+    Class declaration
+        The VarInit class contains the main method.
+    Main Method:
+        Variable declaration:
+            int x; declares an integer variable x that is accessible throughout the main method.
+        For Loop:
+            for(x = 0; x < 3; x++)
+            starts a loop that runs three times (x values from 0 to 2).
+        Block Inside the Loop:
+            int y = -1; initializes the variable y with the value -1 every time the block is entered.
+            Systemm.out.println("y is: " + y);
+                prints the current value of y, which is -1.
+            y = 100; sets the value of y to 100.
+                System.out.println("y is now: " + y); prints the updated value of y, which is 100
+    Key Points:
+        x is declared outside the accessible throughout the loop.
+        y is declared inside the loop block and is reinitialized to -1 every time the block is entered.
+    Loop Behavior:
+        The loop runs three times. Each time, y is set to -1, printed, then set to 100, and printed again.
+
+ */
