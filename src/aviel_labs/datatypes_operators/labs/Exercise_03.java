475d9ccb35c08c587958d8b2120587a8e04f1df1
a2davl1 <avielarenas1@gmail.com>
2024-10-11 12:18:50 -0400

    Commiting completed Exercise_03.java

--- src/aviel_labs/datatypes_operators/labs/Exercise_03.java
+++ src/aviel_labs/datatypes_operators/labs/Exercise_03.java
@@ -11,7 +11,24 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        // Addition
+        int additionResult = 10 + 5;
+        System.out.println("Addition (10 + 5): " + additionResult);
 
+        // Subtraction
+        int subtractionResult = 10 - 5;
+        System.out.println("Subtraction (10 - 5): " + subtractionResult);
+
+        // Multiplication
+        int multiplicationResult = 10 * 5;
+        System.out.println("Multiplication (10 * 5): " + multiplicationResult);
+
+        // Division
+        int divisionResult = 10 / 5;
+        System.out.println("Division (10 / 5): " + divisionResult);
+
+        // Modulus
+        int modulusResult = 10 % 5;
+        System.out.println("Modulus (10 % 5): " + modulusResult);
     }
-
 }
