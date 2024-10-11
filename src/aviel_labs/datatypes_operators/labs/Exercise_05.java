e07e6777dd21bf27bbf135e7957d7030faba8220
a2davl1 <avielarenas1@gmail.com>
2024-10-11 12:23:06 -0400

    Commiting completed Exercise_05.java

--- src/aviel_labs/datatypes_operators/labs/Exercise_05.java
+++ src/aviel_labs/datatypes_operators/labs/Exercise_05.java
@@ -13,13 +13,35 @@ class LogicalOperators {
         // example of "OR"
         boolean a = true;
         boolean b = false;
-        if (a | b){
+        if (a | b) {
             System.out.println("a or b is true");
         }
 
         // write your code below
+        // example of "short-circuit OR"
+        if (a || b) {
+            System.out.println("a or b (short-circuit) is true");
+        }
 
+        // example of "AND"
+        if (a & b) {
+            System.out.println("a and b is true");
+        }
+
+        // example of "short-circuit AND"
+        if (a && b) {
+            System.out.println("a and b (short-circuit) is true");
+        }
+
+        // example of "XOR"
+        if (a ^ b) {
+            System.out.println("a xor b is true");
+        }
+
+        // example of "NOT"
+        if (!a) {
+            System.out.println("not a is true");
+        }
     }
-
 }
 
