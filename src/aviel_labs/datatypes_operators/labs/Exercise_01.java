512575ff23bf7ffd03c429062e94c25231ed3a9c
a2davl1 <avielarenas1@gmail.com>
2024-10-11 12:14:31 -0400

    Commiting completed Exercise_01.java

--- src/aviel_labs/datatypes_operators/labs/Exercise_01.java
+++ src/aviel_labs/datatypes_operators/labs/Exercise_01.java
@@ -17,9 +17,22 @@ public class Exercise_01 {
     public static void main(String[] args) {
 
         // write code here
+        // Step 1: Declare an int, a double, a float, and a char
+        int myInt;
+        double myDouble;
+        float myFloat;
+        char myChar;
 
-    }
+        // Step 2: Assign appropriate values to each
+        myInt = 25;
+        myDouble = 3.14159;
+        myFloat = 9.81f;
+        myChar = 'A';
+
+        // Step 3: Print out each variable to the console
+        System.out.println("Integer: " + myInt);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Char: " + myChar);
+        }
 }
-
-
-
