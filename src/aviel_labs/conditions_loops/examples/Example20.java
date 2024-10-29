9e3674c9a82ea0d752196f83d9dea8eb218318f4
a2davl1 <avielarenas1@gmail.com>
2024-10-29 18:08:12 -0400

    Commiting notes taken on aviel_labs\conditions_loops\Exercise20.java

--- src/aviel_labs/conditions_loops/examples/Example20.java
+++ src/aviel_labs/conditions_loops/examples/Example20.java
@@ -13,3 +13,28 @@ class WhileDemo {
         }
     }
 }
+
+/*
+This demonstrates the use of a while loop
+    to print the alphabet
+
+Class Declaration:
+    class WhileDemo {
+Main Method:
+    public static void man(String args[]) {
+Variable Declaration Line: char ch;
+    Variable: ch
+    Variable Type: char
+Initialize Character: ch = 'a';
+    initializes the variable ch to the character 'a'.
+While Loop: while (ch <= 'z')
+    creates a loop that will run as long as ch is less than or equal to the character 'z'.
+Print Character: System.out.print(ch);
+Increment Character: ch++;
+    increments the value of ch to the next character in the Unicode sequence.
+
+    This loop starts with the character 'a'
+    and continues printing each character until it reaches 'z'.
+    Simple yet effective way to print the alphabet in lowercase!
+
+*/
